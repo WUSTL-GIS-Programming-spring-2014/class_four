b6f222588526c82ce9980798c8150f04e8be3c9e
St Louis County <blord-castillo@stlouisco.com>
2014-02-05 14:18:31 -0600

    Create classfour.py
    Demonstrating variable scope with the module symbol table.

---
+++ classfour.py
@@ -0,0 +1,8 @@
+a = 0
+def myName():
+    print "When called by a function in my namespace, my name is", __name__
+def whatisa():
+    print a
+def whatisglobala():
+    print a
+print "Right now, my name is:", __name__
